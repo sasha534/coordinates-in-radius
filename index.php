fd0bfca827f1881e027f6c907ad37aa13935f5c1
Oleksandr Sorokin <sasha534@bigmir.net>
2020-09-07 22:06:27 +0300

    add js

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-
 </head>
 <body>
 <?php
@@ -19,18 +18,44 @@
         $coordinates = explode(',', $a['coordinates']);
         $a['latitude'] = $coordinates['0'];
         $a['longitude'] = $coordinates['1'];
-//        var_dump($a);
     });
-    array_shift($csv); # remove column header
-
-    $csv = json_encode($csv);
-
-    echo '<script>
-        const posts ='.$csv.';</script>';
-
-
-
+    array_shift($csv);
+    echo "<script> const posts =". json_encode($csv). ";</script>";
 ?>
 
+<script>
+
+            function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+            const R = 6371; // Radius of the earth in km
+            const dLat = deg2rad(lat2-lat1); // deg2rad below
+            const dLon = deg2rad(lon2-lon1);
+            const a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            const d = R * c; // Distance in km
+            return d;
+            }
+
+            function deg2rad(deg) {
+            return deg * (Math.PI/180);
+            }
+
+            function findClosePosts(location, radius, posts) {
+            return posts.filter((post) =>
+            // find close points within the radius of the location, but exclude the location itself from results
+            getDistanceFromLatLonInKm(location.latitude, location.longitude, post.latitude, post.longitude) <= radius && location !== post);
+            }
+
+            function findLocationByName(name, posts) {
+            return posts.find((post) => post.name === name);
+            }
+
+            const hamburg = findLocationByName('Palermo', posts);
+            const closePosts = findClosePosts(hamburg, 400, posts);
+            console.log(closePosts);
+</script>
 </body>
 </html>
